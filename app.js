9abbd9171ed0334686094fc5145ab228fc7762af
ilievZlatko <iliev.zlatko@gmail.com>
2020-09-08 16:58:19 +0300

    commit current progress

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -12,7 +18,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // MIDDLEWARES
-app.use(express.json());
+app.use(helmet());
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
